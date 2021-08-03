217974e82cb0688ba29bae81e79082d6a721f9b5
Richard Macdonald <richard.macdonald@usu.edu>
2021-08-03 13:53:39 -0600

    Created baseline python development environment for USU projects
    Environment includes:
    - unprivileged container user
    - configured instant client on system path
    - python developer tools for debugging, linting, etc

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 
 
+Must be connected to USU Staff VPN for instant-client image download from Harbor.
+
+
 Install Remote-Containers Extension for VSCode
 
 https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers
---
+++ app.py
@@ -0,0 +1,58 @@
+from dotenv import load_dotenv
+from flask import Flask
+import os
+
+load_dotenv()  # load environment variables from .env.
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return app.send_static_file("index.html")
+
+
+@app.route("/db_test/<username>", methods=["GET"])
+def db_test(username):
+    import cx_Oracle
+    conn = cx_Oracle.connect(dsn=os.environ.get("ORACLE_DB_URL"),
+                             user=os.environ.get("ORACLE_DB_USER"),
+                             password=os.environ.get("ORACLE_DB_PASS"),
+                             encoding="UTF-8",
+                             nencoding="UTF-8")
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT
+            spriden_pidm
+        FROM
+            spriden
+        WHERE spriden_change_ind IS null
+            AND spriden_id = :uname
+    """, uname=username)
+    rows = cursor.fetchall()
+
+    return {
+        "username": username,
+        "pidm": rows[0][0]
+    }
+
+
+@app.route("/env_test", methods=["GET"])
+def env_test():
+    test_val = os.environ.get(
+        "ENV_TEST_VALUE", "The environment test value failed to load")
+    return {
+        "test_val": test_val
+    }
+
+# Runnable code snippets. #### --------------------------------------------------------------------------------------
+
+# Highlight the code below, right-click, select 'Run Code' from the context menu.
+
+
+def roll_the_dice():
+    from random import randint
+    print("Rolled: {}".format(randint(1, 6)))
+
+
+roll_the_dice()
